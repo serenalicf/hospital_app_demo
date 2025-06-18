963fdab4ab634c43cf56227dbc7a7c9fcd0e513b
serenali <aserena1103@gmail.com>
2025-06-18 17:40:54 +0800

    add test case for mybatis

--- hospital-management-system/src/main/java/com/serena/hospitalmanagementsystem/entity/User.java
+++ hospital-management-system/src/main/java/com/serena/hospitalmanagementsystem/entity/User.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.Version;
 import lombok.Data;
 
 import java.util.Date;
@@ -21,4 +23,14 @@ public class User {
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
+
+    @Version
+    @TableField(fill = FieldFill.INSERT)
+    private Integer version;
+
+    //implement logical deletion
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
+    private Integer isDeleted;
+
 }
--- hospital-management-system/src/test/java/com/serena/hospitalmanagementsystem/HospitalManagementSystemApplicationTests.java
+++ hospital-management-system/src/test/java/com/serena/hospitalmanagementsystem/HospitalManagementSystemApplicationTests.java
@@ -1,19 +1,24 @@
 package com.serena.hospitalmanagementsystem;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.serena.hospitalmanagementsystem.entity.User;
 import com.serena.hospitalmanagementsystem.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class HospitalManagementSystemApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
-    
+
     @Test
     void contextLoads() {
     }
@@ -27,9 +32,9 @@ class HospitalManagementSystemApplicationTests {
     @Test
     public void testAdd() {
         User user = new User();
-        user.setName("lucy");
-        user.setAge(20);
-        user.setEmail("lucy@serena.com");
+        user.setName("Derick");
+        user.setAge(28);
+        user.setEmail("derick@serena.com");
 
         int newUser = userMapper.insert(user); // no. of affected row
         System.out.println(newUser);
@@ -45,6 +50,64 @@ class HospitalManagementSystemApplicationTests {
     }
 
 
+    //test optimistic lock
+    @Test
+    public void testOptimisticLock() {
+        //query by id
+        User user = userMapper.selectById(1935240912540024833L);
+        //update
+        user.setName("Ben");
+        userMapper.updateById(user);
+    }
 
+
+    @Test
+    public void testSelectByBatchIds() {
+        List<User> userList = userMapper.selectBatchIds(Arrays.asList(1, 2));
+        System.out.println(userList);
+    }
+
+    @Test
+    public void testSelectByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "Matthew");
+        map.put("age", 22);
+        List<User> userList = userMapper.selectByMap(map);
+        System.out.println(userList);
+    }
+
+    //test pagination
+    @Test
+    public void testSelectByPage() {
+        Page<User> page = new Page<>(1, 5);
+        Page<User> userPage = userMapper.selectPage(page, null);
+        long totalPages = userPage.getPages();
+        long currentPage = userPage.getCurrent();
+        List<User> userList = userPage.getRecords();
+        long totalRecords = userPage.getTotal();
+        boolean hasNext = userPage.hasNext();
+        boolean hasPrevious = userPage.hasPrevious();
+        System.out.println(totalPages);
+        System.out.println(currentPage);
+        System.out.println(userList);
+        System.out.println(totalRecords);
+        System.out.println(hasNext);
+        System.out.println(hasPrevious);
+    }
+
+    @Test
+    public void testDeleteById() {
+        int result = userMapper.deleteById(1935252631840739329L);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testQueryWrapper() {
+        //ge / le / lt /gt
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.ge("age", 21);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        System.out.println(userList);
+    }
 
 }
